d4daabdab306d3240d82afaa67c2ed3d3bd4d3bb
AlejandroMontoyaV <alemova.1603@gmail.com>
2025-12-09 21:18:48 -0500

    correccion

--- prueba.py
+++
@@ -1,7 +0,0 @@
-from src.utils.pdf_to_text import pdf_to_text
-
-file_name = "hola.txt"
-file_path = "data/new_docs/hola.txt"
-docs_path = "data/raw_docs"
-txt_path = pdf_to_text(file_name, file_path, docs_path)
-print(f"Archivo final en: {txt_path}")
